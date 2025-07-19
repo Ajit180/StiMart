c45ebfe11c00160b25b616501af478fe75cc956c
Ajit <yajit1908@gmail.com>
2025-07-20 00:00:47 +0530

    deleted the frontend and reinstalled

--- Stimart-Frontend/app/Products/page.jsx
+++
@@ -1,9 +0,0 @@
-
-import ProductList from '@/components/product/productlist'
-import React from 'react'
-
-const page = () => {
-  return <ProductList/>
-}
-
-export default page
--- Stimart-Frontend/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['cart-11.s3.eu-north-1.amazonaws.com'],
-      },
-};
-
-export default nextConfig;
