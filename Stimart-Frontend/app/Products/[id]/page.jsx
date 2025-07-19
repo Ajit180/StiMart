76bf3e4bceb0aea421ccbe914e81432b099394df
Ajit <yajit1908@gmail.com>
2025-07-19 22:20:00 +0530

    await issue in the fetching the id from params

--- Stimart-Frontend/app/Products/[id]/page.jsx
+++ Stimart-Frontend/app/Products/[id]/page.jsx
@@ -1,13 +1,15 @@
 import { getProductById } from "@/apis/product";
 import ProductClient from "@/components/Product/Product";
+import { notFound } from "next/navigation";
 
 
 const SingleProductPage = async ({ params }) => {
-  const { id } = params;
+  const { id } = await params;
   const res = await getProductById(id);
   const product = res?.data;
 
-  if (!product) return <div>Product not found</div>;
+  // if (!product) return <div>Product not found</div>;
+   if (!product) notFound(); // ✅ This will render the 404 page instead of breaking
 
   return <ProductClient product={product} />;
 };
